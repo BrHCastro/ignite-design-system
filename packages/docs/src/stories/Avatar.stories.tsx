525c9613404d49b134ad75a0d7a46495a53fd989
Henrique de Castro <hendecastro@gmail.com>
2022-12-25 23:32:51 -0300

    feat: created avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@system-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://source.unsplash.com/random/300×300/?profile',
+    alt: 'random avatar profiles',
+  },
+} as Meta<AvatarProps>
+
+export const Default: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
