f2984f59447c274faa0fcf0f0b1b75d98ce3d2ec
Henrique de Castro <hendecastro@gmail.com>
2022-12-26 23:49:06 -0300

    feat: created multistep component

---
+++ packages/docs/src/stories/Multistep.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Multistep, MultistepProps } from '@system-ui/react'
+
+export default {
+  title: 'Forms/Multi Step',
+  component: Multistep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultistepProps>
+
+export const Default: StoryObj<MultistepProps> = {}
+
+export const Full: StoryObj<MultistepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
---
+++ packages/react/src/components/Multistep/index.tsx
@@ -0,0 +1,22 @@
+import { Label, MultistepContainer, Step, Steps } from './styled'
+
+export interface MultistepProps {
+  size: number
+  currentStep?: number
+}
+
+export function Multistep({ size, currentStep = 1 }: MultistepProps) {
+  return (
+    <MultistepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultistepContainer>
+  )
+}
